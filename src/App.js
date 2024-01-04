45e09b396436d09460ac76bd1969d7529bfbb98e
Shivam Awasthi <shivamhits1972@gmail.com>
2024-01-04 17:25:46 +0530

    code brush-up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import Header from './container/Header/header'
+import Footer  from './container/footer/footer.jsx'
 
 function App() {
   return (
     <div>
+      {/* homepage */}
       <Header />
+      <Footer />
     </div>
   )
 }
