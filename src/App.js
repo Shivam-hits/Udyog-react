3d9d88b7e59a0f8a54d5be67b77cfdf1dfe92f09
Shivam Awasthi <shivamhits1972@gmail.com>
2024-01-09 17:26:36 +0530

    Landing page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import Header from './container/Header/header'
 import Footer  from './container/footer/footer.jsx'
+import Landing from "../src/Pages/landing"
 
 function App() {
   return (
     <div>
       {/* homepage */}
+      <Landing/>
       <Header />
       <Footer />
+
     </div>
   )
 }
 
-export default App
+export default App;
