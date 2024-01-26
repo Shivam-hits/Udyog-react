8db9256bbce0b45c5d86c351a671697d88c4915e
Shivam Awasthi <shivamhits1972@gmail.com>
2024-01-26 23:34:20 +0530

    testimonial & cta

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1424,28 +1424,11 @@ function HomePage() {
                 
 
 
-
                 {/* <!--
                   - CTA
                 --> */}
 
-                <div class="cta-container">
-
-                  <img src="./new assets/images/CTA banne.png" alt="summer collection" class="cta-banner" />
-
-                  <a href="#" class="cta-content">
-
-                    <p class="discount">10%</p>
-
-                    <h2 class="cta-title">New Openings Sale</h2>
-
-                    <p class="cta-text">Starting @ 10% CD</p>
-
-                    <button class="cta-btn">Order now</button>
-
-                  </a>
-
-                </div>
+                
 
 
 
